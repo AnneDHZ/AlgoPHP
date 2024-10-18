f2ee7a6eed5c2320dd4e3e44be72f4c0ac07de8f
AnneDHZ <anne.dehaeze@yahoo.com>
2024-10-18 15:41:14 +0200

    exo7

---
+++ Partie2/exo7.php
@@ -0,0 +1,40 @@
+<h1>Exercice </h1>
+<P>Créer une fonction personnalisée permettant de générer des cases à cocher. On pourra préciser 
+dans le tableau associatif si la case est cochée ou non.<br>
+Exemple :
+genererCheckbox($elements);
+//où $elements est un tableau associatif clé => valeur avec 3 entrées.
+</P>
+<H2>Résultat</H2>
+
+
+<?php
+
+
+
+$elements = ["choix 1", "choix 2", "choix 3"];
+    
+
+ echo genererCheckbox($elements);
+
+
+function genererCheckbox($elements){
+        $resultat ="<form>";
+
+      foreach ($elements as $choix){
+            $resultat .= "<div>
+                            <input type='checkbox' id ='$choix' name='$choix'/>
+                            <label for='$choix'>$choix</label>
+                           </div> ";
+      }
+      $resultat .="</form>";
+        return $resultat;                                    
+      
+
+};
+
+   
+
+   
+?>
+
