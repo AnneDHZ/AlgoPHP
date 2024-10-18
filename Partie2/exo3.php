a372f779b158871103678fa32071b82d9728b8e9
AnneDHZ <anne.dehaeze@yahoo.com>
2024-10-18 10:23:38 +0200

    exo3

---
+++ Partie2/exo3.php
@@ -0,0 +1,18 @@
+<h1>Exercice </h1>
+<P>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
+dans un nouvel onglet (_blank)<br>
+</P>
+<H2>Résultat</H2>
+
+
+<?php
+
+echo "<p><a href = https://elan-formation.fr/accueil target='_blanck'> Lien vers Elan</a> </p>";
+
+
+
+
+
+
+
+?>
